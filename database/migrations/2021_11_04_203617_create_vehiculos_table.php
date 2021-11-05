58a70cc102874d894f8d6f4ed560b58ee160bb5f
Juliot nuñez <nunezjuliot@gmail.com>
2021-11-04 19:23:47 -0500

    first commit

---
+++ database/migrations/2021_11_04_203617_create_vehiculos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->id();
+            $table->string('placa')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('color')->nullable();
+            $table->string('estado_vehiculo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+}
